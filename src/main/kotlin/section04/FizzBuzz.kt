35ddcca3aa40c0352711bb2bbd32cf7c5a48a838
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-03-19 23:21:17 -0300

    fizzbuzz and test 2

--- src/main/kotlin/section04/FizzBuzz.kt
+++ src/main/kotlin/section04/FizzBuzz.kt
@@ -6,7 +6,7 @@ fun main() {
     println(list)
 }
 
-fun fizzBuzz(n: Int): List<String> {
+fun fizzBuzz1(n: Int): List<String> {
 
     val list = mutableListOf<String>()
 
@@ -15,9 +15,23 @@ fun fizzBuzz(n: Int): List<String> {
             counter % 3 == 0 && counter % 5 == 0 -> "FizzBuzz"
             counter % 3 == 0 -> "Fizz"
             counter % 5 == 0 -> "Buzz"
-            else -> {
-                counter.toString()
-            }
+            else -> counter.toString()
+        }
+        list.add(element)
+    }
+    return list
+}
+
+fun fizzBuzz(n: Int): List<String> {
+
+    val list = mutableListOf<String>()
+
+    (1..n).forEach() {
+        val element = when {
+            it % 3 == 0 && it % 5 == 0 -> "FizzBuzz"
+            it % 3 == 0 -> "Fizz"
+            it % 5 == 0 -> "Buzz"
+            else -> it.toString()
         }
         list.add(element)
     }
